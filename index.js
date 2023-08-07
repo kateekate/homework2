e7bbe9cc8fd547de41d3d08f82e7439ae5f2752c
Katya <katya.mikhaylova.98@list.ru>
2023-08-07 14:00:55 +0200

    refactor: kept second solution of the first task, removed the first one

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
 // Task 1
 
 function makeObjectDeepCopy(sourceObj) {
-  let copyObj;
-
-  if (Array.isArray(sourceObj)) {
-    copyObj = [];
-  } else if (typeof sourceObj === 'object' && !(sourceObj instanceof Date)) {
-    copyObj = {};
-  } else {
-    return sourceObj;
-  }
-
-  for (let key in sourceObj) {
-    if (sourceObj[key] instanceof Date) {
-      copyObj[key] = new Date(sourceObj[key])
-    } else if (Array.isArray(sourceObj[key])) {
-      copyObj[key] = sourceObj[key].map((item) => item instanceof Object ? makeObjectDeepCopy(item) : item)
-    } else if (sourceObj[key] instanceof Object) {
-      copyObj[key] = makeObjectDeepCopy(sourceObj[key]);
-    } else {
-      copyObj[key] = sourceObj[key];
-    }
-  }
-  return copyObj;
-}
-
-// Version 2 improved
-
-function makeObjectDeepCopy(sourceObj) {
   if (Array.isArray(sourceObj)) {
     return sourceObj.map(item => makeObjectDeepCopy(item));
   }
